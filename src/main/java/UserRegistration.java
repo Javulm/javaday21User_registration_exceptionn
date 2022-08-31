e39605b6b0330a73908db960957cde4e6edf452a
Javul Mulla <javulmulla8@gmail.com>
2022-08-31 20:11:51 +0530

    Added: User Register program with test cases.

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -1,3 +1,85 @@
-public class UserRegistration {
+import java.util.regex.Pattern;
 
+public class UserRegistration {
+    public static String isValidFirstName(String firstName) {
+        Pattern pattern = Pattern.compile("^[A-Z][a-zA-z0-9]{2,}$");
+/*		Regex to check valid username and compile the regex
+		Pattern class contains matcher() method to find matching between given username
+		and regular expression.*/
+        if(pattern.matcher(firstName).matches()){
+            return "Happy";
+        }else
+            return "Sad";
+//		Return happy if the firstname matched the Regex
+    }
+    public static String isValidLastName(String lastName) {
+        Pattern pattern = Pattern.compile("^[A-Z][a-zA-z0-9]{2,}$");
+        if(pattern.matcher(lastName).matches()){
+            return "Happy";
+        }else
+            return "Sad";
+    }
+    public static String isValidPhoneNo(String phoneNo) {
+        Pattern pattern = Pattern.compile("^[0-9]{1,2} \\d{10}$");
+        if(pattern.matcher(phoneNo).matches()){
+            return "Happy";
+        }else
+            return "Sad";
+    }
+    public static String isValidEmail(String email) {
+        Pattern pattern = Pattern.compile("^[a-zA-Z0-9]+([+_.-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?");
+        if(pattern.matcher(email).matches()){
+            return "Happy";
+        }else
+            return "Sad";
+    }
+    public static String  isValidPassword1(String password1) {
+        Pattern pattern = Pattern.compile("^[a-zA-z0-9]{8,}$");
+        if(pattern.matcher(password1).matches()){
+            return "Happy";
+        }else
+            return "Sad";
+    }
+    public static String isValidPassword2(String password2) {
+        Pattern pattern = Pattern.compile("^[A-Z][a-zA-z0-9]{7,}$");
+        if(pattern.matcher(password2).matches()){
+            return "Happy";
+        }else
+            return "Sad";
+    }
+    public static String isValidPassword3(String password3) {
+        Pattern pattern = Pattern.compile("^[A-Z][a-zA-z1-9]{6,}[1-9]+$");
+        if(pattern.matcher(password3).matches()){
+            return "Happy";
+        }else
+            return "Sad";
+    }
+    public static String isValidPassword4(String password4) {
+        Pattern pattern = Pattern.compile("^[A-Z][a-zA-z1-9]{5,}[@$^][1-9]+$");
+        if(pattern.matcher(password4).matches()){
+            return "Happy";
+        }else
+            return "Sad";
+    }
+    public static boolean isEmailValidation(String email) {
+        Pattern pattern = Pattern.compile("^[a-zA-Z0-9]+([+_.-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?");
+        return pattern.matcher(email).matches();
+    }
+    /*
+     * create a main method , all program execute in main method
+     * @param args no arguments
+     */
+
+    public static void main(String[] args) {
+//		printing boolean value whether pattern matches given info
+        System.out.println(isValidFirstName("Javul"));
+        System.out.println(isValidLastName("Mulla"));
+        System.out.println(isValidPhoneNo("91 9876543210"));
+        System.out.println(isValidEmail("javulmulla8@gmail.com"));
+        System.out.println(isValidPassword1("javul121"));
+        System.out.println(isValidPassword2("Javul121"));
+        System.out.println(isValidPassword3("Javulmulla1"));
+        System.out.println(isValidPassword4("Javulm$1"));
+        System.out.println(isEmailValidation("javulmulla123@yahoo.co.in"));
+    }
 }
--- src/test/java/UserRegistrationTest.java
+++ src/test/java/UserRegistrationTest.java
@@ -1,3 +1,93 @@
-public class UserRegistrationTest {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class UserRegistrationTest {
+    String result;
+    @Test
+    public void firstName_IsValid_PassTest() {
+        result = UserRegistration.isValidFirstName("Javul");
+        Assert.assertEquals("Happy", result);
+    }
+    @Test
+    public void firstName_IsValid_FailTest() {
+        result = UserRegistration.isValidFirstName("Javul1.23j");
+        Assert.assertEquals("Sad",result);
+    }
+
+    @Test
+    public void lastName_IsValid_PassTest() {
+        result = UserRegistration.isValidLastName("Mulla");
+        Assert.assertEquals("Happy",result);
+    }
+    @Test
+    public void lastName_IsValid_FailTest() {
+        result = UserRegistration.isValidLastName("Mulla.1");
+        Assert.assertEquals("Sad",result);
+    }
+
+    @Test
+    public void Email_IsValid_PassTest() {
+        result = UserRegistration.isValidEmail("javullmulla8@gmail.co.in");
+        Assert.assertEquals("Happy",result);
+    }
+
+    @Test
+    public void Email_IsValid_FailTest() {
+        result = UserRegistration.isValidEmail("javulmulla8.gmail.co.in1");
+        Assert.assertEquals("Sad",result);
+    }
+
+    @Test
+    public void phoneNumber_isValidMobileNo() {
+        result = UserRegistration.isValidPhoneNo("91 9876543210");
+        Assert.assertEquals("Happy",result);
+    }
+
+    @Test
+    public void phoneNumber_IsValid_FailTest() {
+        result = UserRegistration.isValidPhoneNo("89 55655668");
+        Assert.assertEquals("Sad",result);
+    }
+
+    @Test
+    public void password_IsValid_PassTest1() {
+        result = UserRegistration.isValidPassword1("javulmulla");
+        Assert.assertEquals("Happy",result);
+    }
+    @Test
+    public void password_IsValid_FailTest1() {
+        result = UserRegistration.isValidPassword1("Jl121");
+        Assert.assertEquals("Sad",result);
+    }
+
+    @Test
+    public void password_IsValid_PassTest2() {
+        result = UserRegistration.isValidPassword2("Javulmulla");
+        Assert.assertEquals("Happy", result);
+    }
+    @Test
+    public void password_IsValid_FailTest2() {
+        result = UserRegistration.isValidPassword2("javulmulla1");
+        Assert.assertEquals("Sad", result);
+    }
+    @Test
+    public void password_IsValid_PassTest3() {
+        result = UserRegistration.isValidPassword3("Javulmulla1");
+        Assert.assertEquals("Happy", result);
+    }
+    @Test
+    public void password_IsValid_FailTest3() {
+        result = UserRegistration.isValidPassword3("Javulmulla");
+        Assert.assertEquals("Sad", result);
+    }
+    @Test
+    public void password_IsValid_PassTest4() {
+        result = UserRegistration.isValidPassword4("Javulm@11");
+        Assert.assertEquals("Happy", result);
+    }
+    @Test
+    public void password_IsValid_FailTest4() {
+        result = UserRegistration.isValidPassword4("Javul1234");
+        Assert.assertEquals("Sad", result);
+    }
 }
